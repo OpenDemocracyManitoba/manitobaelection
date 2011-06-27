43f4024b5989b5309eb0d05db6a3c73eb113ea57
Wally Glutton <stungeye@gmail.com>
2011-06-27 19:47:30 +0000

    First post!

---
+++ README.md
@@ -0,0 +1,14 @@
+## ManitobaElection.ca
+
+ManitobaElection.ca is a citizen driven website created for the 2011 Manitoba provincial election. Our mission is to provide Manitoba voters with information on their MLA candidates.
+
+The website is powered by:
+
+* Ruby on Rails
+* MySQL
+* Apache with Phushion Passenger
+* Google News
+
+## License
+
+This is free and unencumbered software released into the public domain.  See LICENSE for details.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manitobaelection::Application.config.secret_token = 'a029670ad42e622e686da190b092ba0394926941fca01c92851d68e62077317d3242e8d22d6bb17966d7d552d59de22bdf1ba21ce2fe7a0eaedb31992a9d5f3e'
