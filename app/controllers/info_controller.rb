ce2da2772d45436284224e5565794be9fea34a3f
Wally Glutton <stungeye@gmail.com>
2011-08-23 23:33:57 -0500

    Modified the landing page to include the mini-maps and an embedded google calendar.

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,9 +1,9 @@
 class InfoController < ApplicationController
   def index
     @days_until_election = Date.parse("2011 October 4") - Date.today
-    @regions = Constituency::REGIONS
     latest_news = NewsArticle.approved.with_mentions_and_politicians.limit(6)
     @latest_news_by_date = latest_news.group_by(&:pretty_date)
+    @maps = Map.all
   end
 
   def regions
