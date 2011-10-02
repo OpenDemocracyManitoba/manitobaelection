e6a45809784e4e9c9955a9c17c3ec1a4e8910226
Wally Glutton <stungeye@gmail.com>
2011-10-01 23:05:52 -0500

    Tweak to the layout of the party show pages.

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -5,7 +5,7 @@ class PartiesController < ApplicationController
 
   def show
     @party = Party.with_politicians_and_constit.order('constituencies.name').find(params[:id])
-    @latest_news_by_date = NewsArticle.approved.with_politicians(@party.politicians).limit(15).group_by(&:pretty_date)
+    @latest_news_by_date = NewsArticle.approved.with_politicians(@party.politicians).limit(20).group_by(&:pretty_date)
     @leader = Politician.where(:party_id => @party, :party_leader => true).first
   end
 end
